7dadc5f4d7a429cd929ac6723d065257fd82336c
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-04-02 18:21:39 +0000

    install express

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from 'express';
+
+const app = express
+const port = 3000
+
+app.get('/',(req, res) => {
+    res.send('Hello Wordl!')
+});
+
+app.listen(port, () => {
+    console log(`Serves is running on http://localhost:${port}`);
+});
